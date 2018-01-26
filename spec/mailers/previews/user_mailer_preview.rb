28e0d44f1b4e5004cd086d4c17db38188433a118
Sahil Mehta <samehta91@gmail.com>
2018-01-25 20:07:09 -0800

    Fix grammatical errors and create referral email template

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -2,4 +2,8 @@ class UserMailerPreview < ActionMailer::Preview
   def welcome_email
     UserMailer.welcome_email(User.first)
   end
+
+  def referral_email
+    UserMailer.referral_email(User.first, {name: 'Lauren Cattano', email: 'lauren.cattano@gmail.com'})
+  end
 end
