b0ef29baad4c6b0fc2ca3075a415d6da9ede4bbe
Sahil Mehta <samehta91@gmail.com>
2018-02-09 14:50:27 -0800

    Make navbar collapse when screen width is less than 992px

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,22 +5,4 @@ class User < ApplicationRecord
   def after_confirmation
     UserMailer.welcome_email(self).deliver
   end
-
-  def before_first_session
-    if (self.appointment == (Time.zone.now - 48.hours))
-      UserMailer.first_session_email(self).deliver
-    end
-  end
-
-  def before_second_session
-    if (self.second_appointment == (Time.zone.now - 48.hours))
-      UserMailer.second_session_email(self).deliver
-    end
-  end
-
-  def before_third_session
-    if (self.third_appointment == (Time.zone.now - 48.hours))
-      UserMailer.third_session_email(self).deliver
-    end
-  end
 end
